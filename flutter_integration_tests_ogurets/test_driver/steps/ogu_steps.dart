d34e607753dd7463cc72c5606b7e1e0bf198a65b
Irina Southwell <irina.southwell@clearpoint.co.nz>
2019-05-21 22:04:06 +1200

    add new folder with a sample project to run cucumber style BDD tests with new Ogurets plugin https://pub.dev/packages/ogurets/versions/3.1.1#-readme-tab-

---
+++ flutter_integration_tests_ogurets/test_driver/steps/ogu_steps.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:ogurets/ogurets.dart';
+import 'package:ogurets_flutter/ogurets_flutter.dart';
+
+class ogu_steps {
+
+  FlutterOgurets _world;
+
+  ogu_steps(this._world);
+
+
+  @Given(r'I expect the {string} to be {string}')
+  void iExpectTheToBe(String input1, String input2) async {
+      final locator = find.byValueKey(input1);
+      var text = await _world.driver.getText(locator, timeout: Duration(seconds: 2));
+      if (text != input2) {
+        throw new Exception("${input2} was not ${text}");
+      }
+    }
+
+
+  @When(r'I tap the {string} button {int} times')
+  void iTapTheButtonTimes(String input1, int input2) async {
+    final locator = find.byValueKey(input1);
+    for (var i = 0; i < input2; i += 1) {
+      await _world.driver.tap(locator);
+    }
+  }
+}
